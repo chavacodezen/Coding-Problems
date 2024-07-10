2588ab5242dc141ed9f1c8c99774075c48587afb
Bryan Chavarria <135180883+chavacodezen@users.noreply.github.com>
2024-07-10 16:19:55 -0600

    Create Viral Advertising.js

---
+++ HackerRank/Viral Advertising.js
@@ -0,0 +1,93 @@
+/* PROBLEM
+HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
+On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
+Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .
+Example
+.
+Day Shared Liked Cumulative
+1      5     2       2
+2      6     3       5
+3      9     4       9
+4     12     6      15
+5     18     9      24
+The progression is shown above. The cumulative number of likes on the  day is .
+
+Function Description
+Complete the viralAdvertising function in the editor below.
+
+viralAdvertising has the following parameter(s):
+int n: the day number to report
+
+Returns
+int: the cumulative likes at that day
+
+Input Format
+A single integer, , the day number.
+
+Constraints
+Sample Input
+3
+Sample Output
+9
+Explanation
+This example is depicted in the following diagram:
+strange ad.png
+ people liked the advertisement on the first day,  people liked the advertisement on the second day and  people liked the advertisement on the third day, so the answer is .
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function viralAdvertising(n) {
+    // Write your code here
+    let shared = 5;
+    let cumulativeLikes = 0;
+
+    for (let i = 1; i <= n; i++) {
+        let liked = Math.floor(shared / 2);
+        cumulativeLikes += liked;
+        shared = liked * 3;
+    }
+
+    return cumulativeLikes;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = viralAdvertising(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
