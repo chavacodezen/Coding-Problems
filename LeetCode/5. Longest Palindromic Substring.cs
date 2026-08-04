3a23faa5d038b6dfc87a6c182517156df261d222
Bryan Chavarria <135180883+chavacodezen@users.noreply.github.com>
2026-08-04 14:59:08 -0600

    Implement longest palindromic substring logic
    Refactor LongestPalindrome method to find the longest palindromic substring by expanding around potential centers.

--- LeetCode/5. Longest Palindromic Substring.cs
+++ LeetCode/5. Longest Palindromic Substring.cs
@@ -18,6 +18,38 @@ s consist of only digits and English letters.
 */
 public class Solution {
     public string LongestPalindrome(string s) {
-        
+        string palindrome = String.Empty;
+        string candidate = String.Empty;
+
+        for(int i=0; i<s.Length; i++) {
+            candidate = Expand(s, i, i);
+            if(candidate.Length > palindrome.Length) {
+                palindrome = candidate;
+            }
+
+            candidate = Expand(s, i, i + 1);
+            if(candidate.Length > palindrome.Length) {
+                palindrome = candidate;
+            }
+        }
+
+        return palindrome;
+    }
+
+    public string Expand(string s, int left, int right) {
+        int start = 0;
+        int maxLength = 0;
+
+        while(left >= 0 && right < s.Length && s[left] == s[right]) {
+            int length = right - left + 1;
+            if(length > maxLength) {
+                start = left;
+                maxLength = length;
+            }
+            left--;
+            right++;
+        }
+
+        return s.Substring(start, maxLength);
     }
 }
